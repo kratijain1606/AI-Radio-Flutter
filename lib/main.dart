7120dcffe4271ed389f03bce2653e1195f06bf1b
Krati Jain <kratijain1600@gmail.com>
2021-08-25 16:45:45 +0530

    First commit AI_radio

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:ai_radio/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:ai_radio/utils/ai_util.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(),
+        body: Stack(
+          children: [
+            VxAnimatedBox()
+                .size(context.screenWidth, context.screenHeight)
+                .withGradient(LinearGradient(
+                    colors: [AIColors.primaryColor1, AIColors.primaryColor2],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight))
+                .make()
+          ],
+        ));
+  }
+}
