c52e475fd18d3768293e052b9219778c79e3ae61
Krati Jain <kratijain1600@gmail.com>
2021-08-26 09:37:21 +0530

    Add play pause button and tag

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:ai_radio/model/radio.dart';
 import 'package:ai_radio/utils/ai_util.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -24,6 +25,7 @@ class _HomePageState extends State<HomePage> {
     final radioJson = await rootBundle.loadString("assets/radio.json");
     radios = MyRadioList.fromJson(radioJson).radios;
     print(radios);
+    setState(() {});
   }
 
   @override
@@ -45,8 +47,73 @@ class _HomePageState extends State<HomePage> {
               elevation: 0,
               backgroundColor: Colors.transparent,
               centerTitle: true,
-            ).h(80).p16()
+            ).h(80).p16(),
+            VxSwiper.builder(
+                itemCount: radios.length,
+                aspectRatio: 1.0,
+                enlargeCenterPage: true,
+                itemBuilder: (context, index) {
+                  final rad = radios[index];
+                  return VxBox(
+                          child: ZStack(
+                    [
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: VxBox(
+                          child:
+                              rad.category.text.uppercase.white.make().px16(),
+                        )
+                            .height(40)
+                            .black
+                            .alignCenter
+                            .withRounded(value: 10)
+                            .make(),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: VStack(
+                          [
+                            rad.name.text.xl3.white.bold.make(),
+                            5.heightBox,
+                            rad.tagline.text.sm.white.semiBold.make()
+                          ],
+                          crossAlignment: CrossAxisAlignment.center,
+                        ),
+                      ),
+                      Align(
+                          alignment: Alignment.center,
+                          child: [
+                            Icon(CupertinoIcons.play_circle,
+                                color: Colors.white),
+                            10.heightBox,
+                            "Double tap to Play".text.gray300.make()
+                          ].vStack())
+                    ],
+                    clip: Clip.antiAlias,
+                  ))
+                      .clip(Clip.antiAlias)
+                      .bgImage(DecorationImage(
+                          image: NetworkImage(rad.image),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.3), BlendMode.darken)))
+                      .border(color: Colors.black, width: 5.0)
+                      .withRounded(value: 60)
+                      .make()
+                      .onInkDoubleTap(() {})
+                      .p16();
+                }).centered(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Icon(
+                CupertinoIcons.stop_circle,
+                color: Colors.white,
+                size: 50,
+              ),
+            ).pOnly(bottom: context.percentHeight * 12)
           ],
+          fit: StackFit.expand,
         ));
   }
 }
